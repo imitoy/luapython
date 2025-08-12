32bf867ce7f7d7af5a9d04afbab8d9dcf579bec6
imitoy <imitoy@outlook.com>
2025-08-12 21:35:50 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,23 +1,9 @@
-
+package.cpath=package.cpath..";./?.so"
+luapython = require("luapython")
 
 luapython:init()
 
 local json=import("json")
 
---local data = dict{name="test",age=18}
---print(json.dumps(data))
-for k, v in pairs(getmetatable(json)) do
-    print(k,v)
-end
-print(type(json))
-local r = json.dumps
-for k, v in pairs(getmetatable(json)) do
-    print(k,v)
-end
-for k, v in pairs(getmetatable(r)) do
-    print(k,v)
-end
-print(r==json)
-print(type(json),getmetatable(json),getmetatable(json).__index)
-print(json.dumps,getmetatable(json))
-return r
+local data = dict{name="test",age=18}
+print(json.dumps(data))
